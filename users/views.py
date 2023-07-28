34d9426967ed1d8dd3504018b1ddd54041fb34e1
Abhishek <vishnuabhishek888@gmail.com>
2023-07-28 12:54:07 +0530

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from users.serializers import *
+
+
+class RegisterView(APIView):
+    http_method_names = ['post']
+
+    def post(self, *args, **kwargs):
+        serializer = UserSerializer(data=self.request.data)
+        if serializer.is_valid():
+            get_user_model().objects.create_user(**serializer.validated_data)
+            data = {"message" : "email added"}
+            return Response(data, status=HTTP_201_CREATED)
+        return Response(status=HTTP_400_BAD_REQUEST, data={'errors': serializer.errors})
+
+
+class EmailTokenObtainPairView(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
+
+
+    
